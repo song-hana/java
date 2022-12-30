c0222b39a964f17cf4cf0ef3264ae7e081bd7dd5
송하나 <songhana369@gmail.com>
2022-12-30 10:22:35 +0900

    1229 수업

---
+++ src/ch05/ex05/case01/Calculator.java
@@ -0,0 +1,17 @@
+package ch05.ex05.case01;
+
+public class Calculator {
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	/*
+	public int add(int x, int y) {
+		return x + y;
+	}
+	*/
+	
+	public int add(int a, int b, int c) {
+		return a + b + c;
+	}
+}
